dc804a84d2b473543b8996843ced990ef1cf808a
carolynrehm <32271621+carolynrehm@users.noreply.github.com>
2020-04-23 11:42:32 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,9 +17,7 @@ sudo yum install -y apache-maven
 echo "maven installed"
 
 echo "installing jenkins"
-sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
-sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
-sudo yum install jenkins -y
+wget https://updates.jenkins-ci.org/stable/latest/jenkins.war
 echo "jenkins installed now starting"
-sudo service jenkins start
+sudo nohup java -jar jenkins.war &
 echo "jenkins started"
