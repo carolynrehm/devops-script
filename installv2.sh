23ba6209d20495b7676a80397140c3c841427f33
carolynrehm <carolynhrehm@gmail.com>
2020-04-24 10:21:28 -0500

    adding v2

---
+++ installv2.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+echo "updating existing packages"
+sudo yum update -y
+
+echo "installing java 8"
+sudo yum install -y java-1.8.0-openjdk-devel
+sudo update-alternatives --config java
+sudo update-alternatives --config javac
+echo "java 8 installed and configured"
+
+echo "installing maven"
+sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
+sudo yum install -y apache-maven
+echo "maven installed"
+
+echo "installing jenkins"
+sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
+sudo rpm --import http://pkg.jenkins.io/redhat/jenkins.io.key
+sudo yum install jenkins -y   
+echo "jenkins installed now starting"
+sudo service jenkins start
+echo "jenkins started"
+
+echo "installing docker"
+sudo amazon-linux-extras install docker
+sudo usermod -a -G docker ec2-user
+sudo usermod -a -G docker jenkins
+sudo chmod 666 /var/run/docker.sock
+echo "starting docker"
+sudo service docker start
+echo "docker started"
+
